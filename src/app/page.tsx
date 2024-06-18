568fb0d05d8cd695d5656423401d88e70632b11a
AbinAntony8888 <119113517+AbinAntony8888@users.noreply.github.com>
2024-06-18 19:48:23 +0530

    create new file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedStories from "@/components/FeaturedStories";
 import MainContent from "@/components/maincontent";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <MainContent/>
+      <FeaturedStories/>
     </div>
   );
 }
---
+++ src/components/FeaturedStories.tsx
@@ -0,0 +1,15 @@
+export default function FeaturedStories(){
+return(
+    <div className="ml-20">
+        <div>
+            <h1 className="font-bold text-xl mt-10 pb-5">Featured Stories</h1>
+        </div>
+        <div>
+            <div className="w-48 h-56 flex ">
+                <img src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg" alt="" />
+                <h3></h3>
+            </div>
+        </div>
+    </div>
+)
+}
