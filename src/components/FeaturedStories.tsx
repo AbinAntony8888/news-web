037355db48691a10f2999bf60753efd3f45afabf
AbinAntony8888 <119113517+AbinAntony8888@users.noreply.github.com>
2024-06-18 20:10:34 +0530

    modified featued files

--- src/components/FeaturedStories.tsx
+++ src/components/FeaturedStories.tsx
@@ -1,15 +1,50 @@
-export default function FeaturedStories(){
-return(
+export default function FeaturedStories() {
+  return (
     <div className="ml-20">
-        <div>
-            <h1 className="font-bold text-xl mt-10 pb-5">Featured Stories</h1>
+      <div>
+        <h1 className="font-bold text-xl mt-10 pb-5">Featured Stories</h1>
+      </div>
+      <div className="flex flex-row gap-24">
+        <div className="w-48 h-60 flex ">
+          <img
+            src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg"
+            alt=""
+          />
+          <h3 className="absolute left-24 pt-40 text-white font-bold w-44">We All Share: atomosperic Circulation</h3>
         </div>
-        <div>
-            <div className="w-48 h-56 flex ">
-                <img src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg" alt="" />
-                <h3></h3>
-            </div>
+
+        <div className="w-48 h-60 flex ">
+          <img
+            src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg"
+            alt=""
+          />
+          <h3 className=" pt-40 absolute pl-10 text-white font-bold w-44">We All Share: atomosperic Circulation</h3>
         </div>
+
+        <div className="w-48 h-60 flex ">
+          <img
+            src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg"
+            alt=""
+          />
+          <h3 className=" pt-40 absolute pl-10 text-white font-bold w-44">We All Share: atomosperic Circulation</h3>
+        </div>
+
+        <div className="w-48 h-60 flex ">
+          <img
+            src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg"
+            alt=""
+          />
+          <h3 className=" pt-40 absolute pl-10 text-white font-bold w-44">We All Share: atomosperic Circulation</h3>
+        </div>
+
+        <div className="w-48 h-60 flex ">
+          <img
+            src="https://5.imimg.com/data5/SELLER/Default/2021/6/YU/MH/KN/29117784/parachute-fabric-500x500.jpg"
+            alt=""
+          />
+          <h3 className=" pt-40 absolute pl-10 text-white font-bold w-44">We All Share: atomosperic Circulation</h3>
+        </div>
+      </div>
     </div>
-)
+  );
 }
